220a3df30853717b25934365a473951a7d0ac08d
mariofp77 <mfernandez@DULX0034>
2015-04-16 17:31:05 +0200

    Optimization routine for atomistic systems - 16-4-2015

--- src/kernel/grompp.c
+++ src/kernel/grompp.c
@@ -188,6 +188,8 @@ static void check_cg_sizes(const char *topfn,t_block *cgs,warninp_t wi)
 }
 
 /* MARIO */
+// First version: Optimization through the fascest oscillation. An averaged
+// oscilaltion can be used also.
 static void adaptive_optimization_scheme1(t_inputrec *ir, real auxiliarperiod2, double dt)
 {
   #define EPS  1e-6
@@ -260,6 +262,8 @@ static void adaptive_optimization_scheme1(t_inputrec *ir, real auxiliarperiod2, 
 /* MARIO */
 
 /* MARIO */
+// Second version: The optimization is done using the GROMACS warning for
+// avoiding physical resonances.
 static void adaptive_optimization_scheme2(t_inputrec *ir, real auxiliarperiod2, double dt)
 {
   #define EPS  1e-6
@@ -274,6 +278,81 @@ static void adaptive_optimization_scheme2(t_inputrec *ir, real auxiliarperiod2, 
   real dt_limit1 = sqrt(2)*auxiliarperiod/twopi; // VV limit of 4.44 steps per oscillational period
   real dt_limit2 = 2*auxiliarperiod/twopi; // VV limit of dt w < 2
   real dt_scaled = 2*dt/dt_warn; // This is the timestep to do the comparison as it is done in the paper
+  //dt_scaled = 2; // This line is here for testing
+  printf("The time-step scaled is %f\n",dt_scaled);
+
+  real dt_trial = 0;
+  da2   = 0.25;
+  drho2 = 0;
+  /* Maximun value of rho for two VV steps concatenated */
+  while (dt_trial < dt_scaled)
+  {
+     dt_trial = dt_trial + 0.01;
+     dt2 = sqr(dt_trial);
+     dt4 = sqr(dt2);
+     daux = sqr(2*sqr(da2)*(0.5-da2)*dt2+4*sqr(da2)-6*da2+1)*1e3;
+     daux = daux/(2-da2*dt2);
+     daux = daux/(2-(0.5-da2)*dt2);
+     daux = daux/(1-da2*(0.5-da2)*dt2);
+     drhoAux = dt4*daux*0.125;
+     if (drhoAux > drho2)
+        drho2 = drhoAux;
+  }
+  /* Maximun value of rho for two VV steps concatenated */
+  
+  dt_trial = -0.01;
+  //dt_trial = dt_scaled - 0.1 -0.01; // This line is here because it might be interesting to look only at some epsilon around the time-step dt_scaled
+
+  da_opt = da2;
+  da1 = 0;
+  while (da1 <= da2)
+  {
+     da1 += 1e-6;
+     while (dt_trial < dt_scaled)
+     {
+        dt_trial = dt_trial + 0.01;
+        dt2 = sqr(dt_trial);
+        dt4 = sqr(dt2);
+        daux = sqr(2*sqr(da1)*(0.5-da1)*dt2+4*sqr(da1)-6*da1+1)*1e3;
+        daux = daux/(2-da1*dt2);
+        daux = daux/(2-(0.5-da1)*dt2);
+        daux = daux/(1-da1*(0.5-da1)*dt2);
+        drhoAux = dt4*daux*0.125;
+        if (drhoAux > drho1)
+           drho1 = drhoAux;
+     }
+     dt_trial = -0.01;
+     if (drho2 > drho1)
+     {
+        drho2 = drho1;
+        da_opt = da1;
+     }
+     drho1 = 0;
+  }
+  ir->dIntA = da_opt;
+  printf("The optimal parameter a is %f\n",da_opt);
+  printf("ADAPTIVE SCHEME for the integration\n\n");
+}
+/* MARIO */
+
+/* MARIO */
+// Third version: The optimization is done using the input time-step. It is
+// useful for physical systems where time-steps of the order of magnitude of
+// 1 fs are used (mainly atomistic systems).
+static void adaptive_optimization_scheme3(t_inputrec *ir, real auxiliarperiod2, double dt)
+{
+  #define EPS  1e-6
+  double dt2,dt4;
+  double da0,da1,da2,da_opt,daux,drho0,drho1,drho2,drhoAux;
+
+  real auxiliarperiod = sqrt(auxiliarperiod2);
+  real twopi = 2*M_PI;
+
+  real dt_warn   = auxiliarperiod/10;
+  real dt_max    = auxiliarperiod/5;
+  real dt_limit1 = sqrt(2)*auxiliarperiod/twopi; // VV limit of 4.44 steps per oscillational period
+  real dt_limit2 = 2*auxiliarperiod/twopi; // VV limit of dt w < 2
+  real dt_scaled = dt*1e3; // This is the timestep to do the comparison as it is done in the paper
   //dt_scaled = 2; // This line is here for testing
   printf("The time-step scaled is %f\n",dt_scaled);
 
@@ -467,7 +546,8 @@ static void check_bonds_timestep(gmx_mtop_t *mtop,t_inputrec *ir,warninp_t wi) /
         printf("The fastest oscillation period found is %f ps\n",sqrt(auxiliarperiod2));
         //printf("The averaged oscillation period found is = %f ps\n",auxiliarperiod2/n); // averaged period
         //adaptive_optimization_scheme1(ir,auxiliarperiod2,dt);
-        adaptive_optimization_scheme2(ir,auxiliarperiod2,dt);
+        //adaptive_optimization_scheme2(ir,auxiliarperiod2,dt);
+        adaptive_optimization_scheme3(ir,auxiliarperiod2,dt);
         //adaptive_optimization_scheme(ir,sqr(auxiliarperiod2/n),dt); // averaged period
     }
     /* MARIO */
